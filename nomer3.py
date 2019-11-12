b30154def6dc60c557627d9712fe4d6878f74d5c
nikeliaaa <56597553+nikeliaaa@users.noreply.github.com>
2019-11-12 17:37:27 +0200

    Add files via upload

--- nomer3.py
+++ nomer3.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
-fig,ax=ply.subplots()
 R=4
 t=(-2*np.pi,2*np.pi,0.1)
 x=R*(t-np.sin(t))
@@ -9,11 +8,12 @@ y=R*(1-np.cos(t))
 fig,ax=plt.subplots()
 anim_objects,=plt.plot([],[],'o')
 xdata,ydata=[],[]
-ax.set_xlim(-2*np.pi,2*np.pi)
-ax.set_ylim(-2*np.pi,2*np.pi)
+ax.set_xlim(-40,40)
+ax.set_ylim(0,10)
 def update(frame):
     xdata.append(R*(frame-np.sin(frame)))
     ydata.append(R*(1-np.cos(frame)))
     anim_objects.set_data(xdata,ydata)
-ani=FuncAnimation(fig,update,frames=1000,interval=300)
+ani=FuncAnimation(fig,update,frames=np.arange(-2*np.pi,2*np.pi,0.1),interval=300)
+plt.plot(x,y)
 ani.save('ani.gif')
